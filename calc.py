d5012af239d8255c890d445d5e5887c01c4c9563
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-25 13:02:09 -0400

    Created Calculator class and created an instance to run

--- calc.py
+++ calc.py
@@ -1,1 +1,25 @@
 import tkinter as tk
+
+
+class Calculator:
+
+    def __init__(self):
+        """
+            1. Create Window for application
+            2. Define dimensions of the app (standard iPhone 8 dimensions)
+            3. Disabled resizing window
+            4. Gave app the title of Calculator
+
+        """
+        self.window = tk.Tk()
+        self.window.geometry("375x667")
+        self.window.resizable(0,0)
+        self.window.title("Calculator")
+
+    # Run the app
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
