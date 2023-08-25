beec8713dd47b52cdf17f07e0edf320f01f04ad6
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-25 13:46:52 -0400

    Added operator buttons

--- calc.py
+++ calc.py
@@ -2,6 +2,11 @@ import tkinter as tk
 
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
+DIGITS_FONT_STYLE = ("Arial", 24, "bold")
+DEFAULT_FONT_STYLE = ("Arial", 20)
+
+OFF_WHITE = "#F8FAFF"
+WHITE = "#FFFFFF"
 LIGHT_GRAY = "#F5F5F5"
 LABEL_COLOR = "#25265E"
 
@@ -24,8 +29,21 @@ class Calculator:
         self.current_expression = "0"
 
         self.display_frame = self.create_display_frame()
+
         self.total_label, self.label = self.create_display_labels()
+
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            0: (4, 2), '.': (4, 1)
+        }
+
+        self.operations = {"/": "\u00F7", "*": "\u00D7", "-": "-", "+": "+"}
+
         self.button_frame = self.create_buttons_frame()
+        self.create_digit_buttons()
+        self.create_operator_buttons()
 
     def create_display_labels(self):
         # Creates a total label and puts it on the display frame
@@ -54,6 +72,25 @@ class Calculator:
         frame.pack(expand=True, fill="both")
 
         return frame
+
+    def create_digit_buttons(self):
+        # Place buttons in button frame
+        # Text is the digit
+        # Value of self.digits is the row, column placement
+        # Buttons fill up every side and fills up grid with sticky argument NSEW
+        # Removed borders
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.button_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE,
+                               borderwidth=0)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def create_operator_buttons(self):
+        i = 0
+        for operator, symbol in self.operations.items():
+            button = tk.Button(self.button_frame, text=symbol, bg=OFF_WHITE, fg=LABEL_COLOR, font=DEFAULT_FONT_STYLE,
+                               borderwidth=0)
+            button.grid(row=i, column=4, sticky=tk.NSEW)
+            i += 1
 
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
