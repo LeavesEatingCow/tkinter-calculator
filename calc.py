06fc6e2f0b66ce9b587e8601a64209b86577e8a5
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-08-25 15:00:47 -0400

    Binded keyboard to buttons

--- calc.py
+++ calc.py
@@ -53,11 +53,22 @@ class Calculator:
         self.create_operator_buttons()
         self.create_specials_buttons()
 
+    def bind_keys(self):
+        # Hitting Enter on keyboard equates to running the evaluate method
+        self.window.bind("<Return>", lambda event: self.evaluate())
+
+        for key in self.digits:
+            self.window.bind(str(key), lambda event, digit=key: self.add_to_expression(digit))
+
+        for key in self.operations:
+            self.window.bind(key, lambda event, operator=key: self.append_operator(operator))
+
     def create_specials_buttons(self):
         self.create_clear_button()
         self.create_equals_button()
         self.create_square_button()
         self.create_sqrt_button()
+        self.bind_keys()
 
     def create_display_labels(self):
         # Creates a total label and puts it on the display frame
